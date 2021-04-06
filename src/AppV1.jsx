23b28994977608a52d40651c914bbb3f4eb18ef7
Hari Mahat <hmahat@gmail.com>
2021-04-06 15:10:59 +0300

    async away with try catch block for data fetching

--- src/AppV1.jsx
+++ src/AppV1.jsx
@@ -88,17 +88,20 @@ const App = () => {
   }, [url]); */
 
   // Data Fetching with axios
-  const handleFetchStories = useCallback(() => {
+  const handleFetchStories = useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({
+        type: 'STORIES_FETCH_FAILURE',
+      });
+    }
   }, [url]);
 
   // using useCallback() in useEffect
